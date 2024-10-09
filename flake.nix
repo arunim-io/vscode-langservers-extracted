b4e86f4fa340a217c1206a05a5e6b91910b2cc6f
Mugdha Arunim Ahmed <mugdhaarunimahmed2017@gmail.com>
2024-10-09 23:19:19 +0600

    add gh action

--- flake.nix
+++ flake.nix
@@ -1,8 +1,18 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     systems.url = "github:nix-systems/default";
-    parts.url = "github:hercules-ci/flake-parts";
+
+    parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
+    gh-actions = {
+      url = "github:nix-community/nix-github-actions";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
@@ -12,27 +22,30 @@
 
       imports = with inputs; [ parts.flakeModules.easyOverlay ];
 
+      flake.githubActions = inputs.gh-actions.lib.mkGithubMatrix {
+        checks = inputs.nixpkgs.lib.getAttrs [ "x86_64-linux" ] inputs.self.checks;
+      };
+
       perSystem =
         { pkgs, config, ... }:
+        let
+          packageName = "vscode-langservers-extracted";
+          pkg = builtins.getAttr packageName config.packages;
+        in
         {
           packages = {
-            default = config.packages.vscode-langservers-extracted;
-            vscode-langservers-extracted = pkgs.callPackage ./default.nix { };
+            default = pkg;
+            ${packageName} = pkgs.callPackage ./default.nix { };
           };
 
-          overlayAttrs = {
-            inherit (config.packages) vscode-langservers-extracted;
-          };
+          overlayAttrs.${packageName} = pkg;
 
-          devShells.default = pkgs.mkShell {
-            buildInputs = with pkgs; [
-              git
-              python3
-              nodejs
-              yarn
-              nodePackages.gulp-cli
-            ];
-          };
+          checks.default = pkg;
         };
     };
+
+  nixConfig = {
+    extra-substituters = [ "https://arunim.cachix.org" ];
+    extra-trusted-public-keys = [ "arunim.cachix.org-1:J07zWDguRFHQSio/VmTT8us5EelRNlDTFkbNeFel0xM=" ];
+  };
 }
